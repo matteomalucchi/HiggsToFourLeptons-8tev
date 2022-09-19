1f5221db618b9066f3528433fde599ae06e4e165
matteomalucchi <matteomalucchi13@gmail.com>
2022-09-19 10:53:12 +0200

    remove _statis

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ autodoc_mock_imports = ["ROOT"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+html_static_path = []
 
 """
 import subprocess
